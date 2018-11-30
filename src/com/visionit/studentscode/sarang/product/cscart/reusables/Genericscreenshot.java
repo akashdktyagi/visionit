ad8a9c1aac872f54d1003bea332ce869ae3743b3
Sarang <Sarang@DESKTOP-EBQA6UF>
2018-11-30 15:22:27 +0530

    test commit

--- src/com/visionit/studentscode/sarang/product/cscart/reusables/Genericscreenshot.java
+++ src/com/visionit/studentscode/sarang/product/cscart/reusables/Genericscreenshot.java
@@ -1,5 +1,21 @@
 package com.visionit.studentscode.sarang.product.cscart.reusables;
 
-public class Genericscreenshot {
+import com.visionit.studentscode.sarang.product.cscart.po.po_ele_demo;
 
+public class Genericscreenshot extends po_ele_demo
+{
+		public static void getScrrenShot(WebDriver driver,String name)
+		{
+			TakesScreenshot shot=(TakesScreenshot)driver;
+			File src = shot.getScreenshotAs(OutputType.FILE);
+			File dest=new File("../visionit/screenshot/"+name+".png");
+			try {
+				FileUtils.copyFile(src, dest);
+			} 
+			catch (IOException e) 
+			{
+				
+				e.printStackTrace();
+			}
+		}
 }
