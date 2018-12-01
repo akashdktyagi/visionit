78a702e837f768a18e4584e46ef777d4ad90d706
Sarang <Sarang@DESKTOP-EBQA6UF>
2018-12-01 16:36:20 +0530

    add

--- src/com/visionit/studentscode/sarang/snippets/Assertions/HardAssertions.java
+++ src/com/visionit/studentscode/sarang/snippets/Assertions/HardAssertions.java
@@ -4,11 +4,10 @@ package com.visionit.studentscode.sarang.snippets.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 
-public class Assertions {
+public class HardAssertions {
 	//Assert is a Class in TestNG which will allows to compare values
 	//Here if assertion has failed then the lines after assertion cannot be executed to that respective @Test so driver.quit..
 	//..will not be executed
@@ -17,7 +16,7 @@ public class Assertions {
 	
 	//Simple Assertion
 	@Test(priority=1)
-	public void Assert1()
+	public void assert1()
 	{
 			System.setProperty("webdriver.chrome.driver", "C:\\Vision\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
@@ -33,9 +32,10 @@ public class Assertions {
 			
 	}
 	
-	//Proper way to use Assertion with Valid Error Message
+	//Proper way to use Assertion with Valid Error Message..
+	//..with comparision of two WebElements
 	@Test(priority=2)
-	public void Assert2()
+	public void assert2()
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Vision\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -46,6 +46,29 @@ public class Assertions {
 		String Exp = "Facebook – log in or sign";
 		String Act = driver.getTitle();
 		Assert.assertEquals(Act, Exp,"Facebook Page is not opened Correctly");
+		driver.quit();
+	}
+	
+	//Use of Assert.assertTrue
+	//It will check the condition and if it gets true then Test Case gets pass
+	@Test(priority=3)
+	public void assert3()
+	{
+		String name = "Sarang";
+		Assert.assertTrue(name.contains("Vaidehi"), "Names Are Different");
+	}
+	
+	@Test(priority=4)
+	public void assert4()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Vision\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/");
+		String Act = driver.getTitle();
+		Assert.assertFalse(Act.contains("Facebook – log in or sign up"), "Facebook Site is opened Successfully");
 		driver.quit();
 	}
 	
