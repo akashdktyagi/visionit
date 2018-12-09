f4f126b56622fc945c6a97b1409b49e57af3be97
Sarang <Sarang@Sarang-Notebook>
2018-12-09 11:28:16 +0530

    Changes in Reusables Generic Screenshot

--- src/com/visionit/studentscode/sarang/product/cscart/reusables/Genericscreenshot.java
+++ src/com/visionit/studentscode/sarang/product/cscart/reusables/Genericscreenshot.java
@@ -3,6 +3,7 @@ package com.visionit.studentscode.sarang.product.cscart.reusables;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
